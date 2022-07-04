7675aa9dc0fe7d3eb6d957f3ec62e3363fa9b9eb
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2022-07-04 15:53:33 +0100

    Refresh repository
    Delete all files except .gitattributes and .gitignore.

--- src/components/states/actors/players/player_state.gd
+++
@@ -1,9 +0,0 @@
-class_name PlayerState
-extends State
-
-# Player State Base
-# A player state is a state that controls a player.
-
-export(NodePath) var _player_path: NodePath = NodePath("../..")
-
-onready var player: Player = get_node(_player_path)
