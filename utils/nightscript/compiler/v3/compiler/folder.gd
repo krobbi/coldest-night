8f162a364e5b5bd640fa4a3ae32492e95b4bdb8a
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-01-13 12:54:49 +0000

    NightScript unary folding
    Add folding for unary expressions.

--- utils/nightscript/compiler/v3/compiler/folder.gd
+++ utils/nightscript/compiler/v3/compiler/folder.gd
@@ -10,6 +10,8 @@ const IntExprASTNode: GDScript = preload("../ast/int_expr_ast_node.gd")
 const ScopeStack: GDScript = preload("scope_stack.gd")
 const StrExprASTNode: GDScript = preload("../ast/str_expr_ast_node.gd")
 const Symbol: GDScript = preload("symbol.gd")
+const Token: GDScript = preload("../lexer/token.gd")
+const UnExprASTNode: GDScript = preload("../ast/un_expr_ast_node.gd")
 
 var scope_stack: ScopeStack
 
@@ -27,10 +29,29 @@ func copy_span(from: ExprASTNode, to: ExprASTNode) -> ExprASTNode:
 
 # Fold an expression AST node.
 func fold_expr(expr: ExprASTNode) -> ExprASTNode:
-	if expr is IdentifierExprASTNode:
+	if expr is UnExprASTNode:
+		return fold_un_expr(expr)
+	elif expr is IdentifierExprASTNode:
 		return fold_identifier_expr(expr)
 	
 	return expr
+
+
+# Fold a unary expression AST node.
+func fold_un_expr(un_expr: UnExprASTNode) -> ExprASTNode:
+	var child_expr: ExprASTNode = fold_expr(un_expr.expr)
+	
+	if not child_expr is IntExprASTNode:
+		return copy_span(un_expr, UnExprASTNode.new(un_expr.operator, child_expr))
+	
+	if un_expr.operator == Token.BANG:
+		return copy_span(un_expr, IntExprASTNode.new(int(child_expr.value == 0)))
+	elif un_expr.operator == Token.PLUS:
+		return copy_span(un_expr, child_expr)
+	elif un_expr.operator == Token.MINUS:
+		return copy_span(un_expr, IntExprASTNode.new(-child_expr.value))
+	
+	return copy_span(un_expr, UnExprASTNode.new(un_expr.operator, child_expr))
 
 
 # Fold an identifier expression AST node.
