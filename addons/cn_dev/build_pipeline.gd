3a25cf06687d789ec6b53ca2955cac00fc7675e6
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-01-15 16:09:08 +0000

    Export cleanup
    Move excluded files to export presets.
    Remove ineffective exclude process from build pipeline.

--- addons/cn_dev/build_pipeline.gd
+++ addons/cn_dev/build_pipeline.gd
@@ -2,56 +2,16 @@ extends EditorExportPlugin
 
 # Coldest Night Development Toolkit Build Pipeline
 # The Coldest Night Development Toolkit build pipeline is an editor export
-# plugin that excludes, transforms, and adds resource files to Coldest Night
-# when it is exported.
+# plugin that transforms resource files when they are exported.
 
-const EXCLUDE_COMMON: PoolStringArray = PoolStringArray([
-	"res://.git/",
-	"res://.idea/",
-	"res://.vscode/",
-	"res://builds/",
-	"res://coldnight_font/",
-	"res://docs/",
-	"res://ignore/",
-	"res://license.txt",
-	"res://logo.png",
-])
-
-const EXCLUDE_RELEASE: PoolStringArray = PoolStringArray([
-	"res://addons/cn_dev/",
-	"res://utils/nightscript/compiler/",
-	"res://utils/nightscript/debug/",
-])
-
-var _is_debug: bool = false
-
-# Run when the game begins exporting. Set whether the game is being exported in
-# debug mode.
-func _export_begin(_features: PoolStringArray, is_debug: bool, _path: String, _flags: int) -> void:
-	_is_debug = is_debug
-
-
-# Run when the game exports a resource file. Exclude, transform, or add resource
-# files where applicable.
+# Run when the game exports a resource file. Transform the resource file if
+# applicable.
 func _export_file(path: String, _type: String, _features: PoolStringArray) -> void:
-	for exclude in EXCLUDE_COMMON:
-		if path.begins_with(exclude):
-			skip()
-			return
-	
-	if _is_debug:
-		return
-	
-	for exclude in EXCLUDE_RELEASE:
-		if path.begins_with(exclude):
-			skip()
-			return
-	
 	if path.match("res://resources/data/nightscript/*.ns"):
 		_pipe_compile_nightscript(path)
 
 
-# Compile a NightScript source file for release mode.
+# Compile a NightScript source file to one or more NightScript bytecode files.
 func _pipe_compile_nightscript(path: String) -> void:
 	skip()
 	
