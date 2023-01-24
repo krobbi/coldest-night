481c276435e3c906984d30d5d8a998dbf97dfbe9
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-01-24 16:06:08 +0000

    Focus camera triggers
    Make focus camera triggers use a node path rather than a level point.

--- entities/triggers/focus_camera_trigger/focus_camera_trigger.gd
+++ entities/triggers/focus_camera_trigger/focus_camera_trigger.gd
@@ -2,24 +2,29 @@ class_name FocusCameraTrigger
 extends Trigger
 
 # Focus Camera Trigger
-# A focus camera trigger is a trigger that focuses the level camera on one of
-# the current level's points when entered by a player.
+# A focus camera trigger is a trigger that focuses the level camera on a node
+# when entered by the player.
 
-export(String) var point: String
+export(NodePath) var _focus_node_path: NodePath
 
-# Virtual _player_enter method. Runs when a player enters the focus camera
-# trigger. Focuses the level camera on one of the current level's points:
+var _focus_node: Node2D = self
+
+# Run when the focus camera trigger enters the scene tree. Set the focus camera
+# trigger's focus node.
+func _ready() -> void:
+	if _focus_node_path and has_node(_focus_node_path):
+		var focus_node: Node = get_node(_focus_node_path)
+		
+		if focus_node is Node2D:
+			_focus_node = focus_node
+
+
+# Run when the player enters the focus camera trigger. Focus the level camera on
+# the focus node.
 func _player_enter(_player: Player) -> void:
-	if Global.tree.current_scene.name != "Overworld":
-		return
-	
-	var current_level: Level = Global.tree.current_scene.level_host.current_level
-	
-	if current_level:
-		Global.events.emit_signal("camera_focus_request", current_level.get_point_pos(point))
+	Global.events.emit_signal("camera_focus_request", _focus_node.global_position)
 
 
-# Virtual _player_exit method. Runs when a player exits the focus camera
-# trigger. Unfocuses the level camera:
+# Run when the player exits the focus camera trigger. Unfocus the level camera.
 func _player_exit(_player: Player) -> void:
 	Global.events.emit_signal("camera_unfocus_request")
