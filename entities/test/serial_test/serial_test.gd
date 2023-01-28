9706753a5d4225cd4741d1b8ab63cef6a6722c30
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-01-28 16:33:24 +0000

    Serial tests
    Create serial test entity.

---
+++ entities/test/serial_test/serial_test.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+# Serial Test
+# A serial test is an entity used to test serialization and deserialization. A
+# serial test can be interacted with to switch between 3 states.
+
+onready var _sprite: Sprite = $Sprite
+
+# Run when the serial test's interactable is interacted with. Toggle the serial
+# test's state.
+func _on_interactable_interacted() -> void:
+	_sprite.frame = (_sprite.frame + 1) % _sprite.hframes
