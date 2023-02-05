9b9ad4670387e332326121a2e5a3f8962adfeee9
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-05 22:02:30 +0000

    NightScript subsequent jump elimination
    Add subsequent jump elimination optimization.
    Change order of optimizations.
    Add comments justifying order of optimizations.

--- nightscript/compiler/backend/optimizer.gd
+++ nightscript/compiler/backend/optimizer.gd
@@ -67,6 +67,11 @@ func is_block_terminated(block: IRBlock) -> bool:
 	return false
 
 
+# Get whether an IR operation is a jump operation.
+func is_op_jump(op: IROp) -> bool:
+	return op.type in [IROp.JUMP_LABEL, IROp.JUMP_ZERO_LABEL, IROp.JUMP_NOT_ZERO_LABEL]
+
+
 # Get whether an IR operation references a label.
 func is_op_label(op: IROp) -> bool:
 	return op.type in [
@@ -100,18 +105,75 @@ func optimize_code(code: IRCode) -> void:
 		iterations -= 1
 		
 		for method in [
-			"optimize_thread_labels",
-			"optimize_merge_subsequent_blocks",
-			"optimize_thread_terminators",
+			# Run optimizations that are confined to a single block. These may
+			# benefit future optimizations, but never have any negative effect.
 			"optimize_eliminate_unreachable_ops",
-			"optimize_eliminate_unreachable_blocks",
 			"optimize_eliminate_push_drops",
+			
+			# Redirect operations that reference an empty block or a jump
+			# operation to simplify control flow.
+			"optimize_thread_labels",
+			
+			# Eliminate unreachable blocks created by the label threading
+			# optimization to avoid blocking future optimizations that depend on
+			# the control flow between blocks.
+			"optimize_eliminate_unreachable_blocks",
+			
+			# Eliminate jump operations that lead directly to the subsequent
+			# block. These are unnecessary and may block future optimizations.
+			"optimize_eliminate_subsequent_jumps",
+			
+			# Merge unreferenced blocks that can only be accessed from the
+			# previous block into the previous block. It is unclear if this is
+			# actually beneficial, but it simplifies the IR code.
+			"optimize_merge_subsequent_blocks",
+			
+			# Replace jumps to terminator operations with a copy of the
+			# terminator operation. This is performed after the control flow
+			# optimizations because it is more efficient to remove a jump and
+			# fall through into the terminator operation if possible.
+			"optimize_thread_terminators",
 		]:
 			var method_iterations: int = 256
 			
 			while call(method, code) and method_iterations > 0:
 				is_optimized = true
 				method_iterations -= 1
+
+# Eliminate IR operations that follow a terminator operation and return whether
+# any optimization was performed.
+func optimize_eliminate_unreachable_ops(code: IRCode) -> bool:
+	var is_optimized: bool = false
+	
+	for block in code.blocks:
+		for terminator_index in range(block.ops.size() - 1):
+			if is_op_terminator(block.ops[terminator_index]):
+				for back_index in range(block.ops.size() - 1, terminator_index, -1):
+					block.ops.remove(back_index)
+				
+				is_optimized = true
+				break
+	
+	return is_optimized
+
+
+# Eliminate pure push IR operations that are immediately followed by a drop IR
+# operation and return whether any optimization was performed.
+func optimize_eliminate_push_drops(code: IRCode) -> bool:
+	var is_optimized: bool = false
+	
+	for block in code.blocks:
+		var index: int = 0
+		
+		while index < block.ops.size() - 1:
+			if is_op_pure_push(block.ops[index]) and block.ops[index + 1].type == IROp.DROP:
+				block.ops.remove(index)
+				block.ops.remove(index)
+				is_optimized = true
+			else:
+				index += 1
+	
+	return is_optimized
 
 
 # Redirect IR operations referencing empty IR blocks or jump operations to the
@@ -139,59 +201,6 @@ func optimize_thread_labels(code: IRCode) -> bool:
 				if is_op_label(op) and get_op_label(op) == block.label:
 					set_op_label(op, target_label)
 					is_optimized = true
-	
-	return is_optimized
-
-
-# Merge subsequent IR blocks if the second IR block is not referenced in IR code
-# and return whether any optimization was performed.
-func optimize_merge_subsequent_blocks(code: IRCode) -> bool:
-	var is_optimized: bool = false
-	
-	for index in range(code.blocks.size() - 1, 0, -1):
-		var block: IRBlock = code.blocks[index]
-		
-		if not is_block_referenced(code, block):
-			code.blocks[index - 1].ops.append_array(block.ops)
-			code.blocks.remove(index)
-			is_optimized = true
-	
-	return is_optimized
-
-
-# Replace jump IR operations to a terminator operation with a copy of the
-# terminator operation and return whether any optimization was performed.
-func optimize_thread_terminators(code: IRCode):
-	var is_optimized: bool = false
-	
-	for block in code.blocks:
-		if block.ops.empty() or not is_op_terminator(block.ops[0]):
-			continue
-		
-		for other_block in code.blocks:
-			for op in other_block.ops:
-				if(
-						op.type == IROp.JUMP_LABEL and get_op_label(op) == block.label
-						and op != block.ops[0]):
-					op.copy(block.ops[0])
-					is_optimized = true
-	
-	return is_optimized
-
-
-# Eliminate IR operations that follow a terminator operation and return whether
-# any optimization was performed.
-func optimize_eliminate_unreachable_ops(code: IRCode) -> bool:
-	var is_optimized: bool = false
-	
-	for block in code.blocks:
-		for terminator_index in range(block.ops.size() - 1):
-			if is_op_terminator(block.ops[terminator_index]):
-				for back_index in range(block.ops.size() - 1, terminator_index, -1):
-					block.ops.remove(back_index)
-				
-				is_optimized = true
-				break
 	
 	return is_optimized
 
@@ -227,20 +236,66 @@ func optimize_eliminate_unreachable_blocks(code: IRCode) -> bool:
 	return is_optimized
 
 
-# Eliminate pure push IR operations that are immediately followed by a drop IR
-# operation and return whether any optimization was performed.
-func optimize_eliminate_push_drops(code: IRCode) -> bool:
+# Eliminate jump IR operations that lead to the subsequent IR block and return
+# whether any optimization was performed.
+func optimize_eliminate_subsequent_jumps(code: IRCode) -> bool:
 	var is_optimized: bool = false
 	
 	for block in code.blocks:
-		var index: int = 0
+		var next_block: IRBlock = get_next_block(code, block)
 		
-		while index < block.ops.size() - 1:
-			if is_op_pure_push(block.ops[index]) and block.ops[index + 1].type == IROp.DROP:
+		if not next_block:
+			continue
+		
+		for index in range(block.ops.size() - 1, -1, -1):
+			var op: IROp = block.ops[index]
+			
+			if is_op_jump(op) and get_op_label(op) == next_block.label:
 				block.ops.remove(index)
-				block.ops.remove(index)
+				
+				# Preserve stack size on conditional jumps.
+				if op.type != IROp.JUMP_LABEL:
+					code.set_label(block.label)
+					code.make_drop()
+				
 				is_optimized = true
 			else:
-				index += 1
+				break
+	
+	return is_optimized
+
+
+# Merge subsequent IR blocks if the second IR block is not referenced in IR code
+# and return whether any optimization was performed.
+func optimize_merge_subsequent_blocks(code: IRCode) -> bool:
+	var is_optimized: bool = false
+	
+	for index in range(code.blocks.size() - 1, 0, -1):
+		var block: IRBlock = code.blocks[index]
+		
+		if not is_block_referenced(code, block):
+			code.blocks[index - 1].ops.append_array(block.ops)
+			code.blocks.remove(index)
+			is_optimized = true
+	
+	return is_optimized
+
+
+# Replace jump IR operations to a terminator operation with a copy of the
+# terminator operation and return whether any optimization was performed.
+func optimize_thread_terminators(code: IRCode) -> bool:
+	var is_optimized: bool = false
+	
+	for block in code.blocks:
+		if block.ops.empty() or not is_op_terminator(block.ops[0]):
+			continue
+		
+		for other_block in code.blocks:
+			for op in other_block.ops:
+				if(
+						op.type == IROp.JUMP_LABEL and get_op_label(op) == block.label
+						and op != block.ops[0]):
+					op.copy(block.ops[0])
+					is_optimized = true
 	
 	return is_optimized
