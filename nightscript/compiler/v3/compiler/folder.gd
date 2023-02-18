bdefb75fc4a11404ac056a8838f11ed2d8bc8193
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-18 15:14:25 +0000

    NightScript expression folder span stack
    Keep track of folded expression spans in stack.

--- nightscript/compiler/v3/compiler/folder.gd
+++ nightscript/compiler/v3/compiler/folder.gd
@@ -16,17 +16,22 @@ const Token: GDScript = preload("../lexer/token.gd")
 const UnExprASTNode: GDScript = preload("../ast/un_expr_ast_node.gd")
 
 var scope_stack: ScopeStack
+var span_stack: Array = []
 
 # Set the folder's scope stack.
 func _init(scope_stack_ref: ScopeStack) -> void:
 	scope_stack = scope_stack_ref
 
 
-# Copy an expression AST node's span to another expression AST node and return
-# the target expression AST node.
-func copy_span(from: ExprASTNode, to: ExprASTNode) -> ExprASTNode:
-	to.span.copy(from.span)
-	return to
+# Push a new span to the span stack at an expression AST node's span.
+func push_span(expr: ExprASTNode) -> void:
+	span_stack.push_back(expr.span.duplicate())
+
+
+# Pop and apply the top span from the span stack to an expression AST node.
+func pop_span(expr: ExprASTNode) -> ExprASTNode:
+	expr.span.copy(span_stack.pop_back())
+	return expr
 
 
 # Fold an expression AST node.
@@ -45,66 +50,68 @@ func fold_expr(expr: ExprASTNode) -> ExprASTNode:
 
 # Fold a unary expression AST node.
 func fold_un_expr(un_expr: UnExprASTNode) -> ExprASTNode:
+	push_span(un_expr)
 	var child_expr: ExprASTNode = fold_expr(un_expr.expr)
 	
 	if not child_expr is IntExprASTNode:
-		return copy_span(un_expr, UnExprASTNode.new(un_expr.operator, child_expr))
+		return pop_span(UnExprASTNode.new(un_expr.operator, child_expr))
 	
 	if un_expr.operator == Token.KEYWORD_NOT:
-		return copy_span(un_expr, IntExprASTNode.new(int(child_expr.value == 0)))
+		return pop_span(IntExprASTNode.new(int(child_expr.value == 0)))
 	elif un_expr.operator == Token.PLUS:
-		return copy_span(un_expr, child_expr)
+		return pop_span(child_expr)
 	elif un_expr.operator == Token.MINUS:
-		return copy_span(un_expr, IntExprASTNode.new(-child_expr.value))
+		return pop_span(IntExprASTNode.new(-child_expr.value))
 	
-	return copy_span(un_expr, UnExprASTNode.new(un_expr.operator, child_expr))
+	return pop_span(UnExprASTNode.new(un_expr.operator, child_expr))
 
 
 # Fold a binary expression AST node.
 func fold_bin_expr(bin_expr: BinExprASTNode) -> ExprASTNode:
+	push_span(bin_expr)
 	var rhs_expr: ExprASTNode = fold_expr(bin_expr.rhs_expr)
 	
 	# Never fold the left hand side of assignments.
 	if bin_expr.operator == Token.EQUALS:
-		return copy_span(
-				bin_expr, BinExprASTNode.new(bin_expr.lhs_expr, bin_expr.operator, rhs_expr))
+		return pop_span(BinExprASTNode.new(bin_expr.lhs_expr, bin_expr.operator, rhs_expr))
 	
 	var lhs_expr: ExprASTNode = fold_expr(bin_expr.lhs_expr)
 	
 	if lhs_expr is IntExprASTNode:
 		if bin_expr.operator == Token.KEYWORD_AND:
-			return copy_span(bin_expr, rhs_expr if lhs_expr.value != 0 else lhs_expr)
+			return pop_span(rhs_expr if lhs_expr.value != 0 else lhs_expr)
 		elif bin_expr.operator == Token.KEYWORD_OR:
-			return copy_span(bin_expr, lhs_expr if lhs_expr.value != 0 else rhs_expr)
+			return pop_span(lhs_expr if lhs_expr.value != 0 else rhs_expr)
 	
 	if not lhs_expr is IntExprASTNode or not rhs_expr is IntExprASTNode:
-		return copy_span(bin_expr, BinExprASTNode.new(lhs_expr, bin_expr.operator, rhs_expr))
+		return pop_span(BinExprASTNode.new(lhs_expr, bin_expr.operator, rhs_expr))
 	
 	if bin_expr.operator == Token.BANG_EQUALS:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value != rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value != rhs_expr.value)))
 	elif bin_expr.operator == Token.STAR:
-		return copy_span(bin_expr, IntExprASTNode.new(lhs_expr.value * rhs_expr.value))
+		return pop_span(IntExprASTNode.new(lhs_expr.value * rhs_expr.value))
 	elif bin_expr.operator == Token.PLUS:
-		return copy_span(bin_expr, IntExprASTNode.new(lhs_expr.value + rhs_expr.value))
+		return pop_span(IntExprASTNode.new(lhs_expr.value + rhs_expr.value))
 	elif bin_expr.operator == Token.MINUS:
-		return copy_span(bin_expr, IntExprASTNode.new(lhs_expr.value - rhs_expr.value))
+		return pop_span(IntExprASTNode.new(lhs_expr.value - rhs_expr.value))
 	elif bin_expr.operator == Token.LESS:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value < rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value < rhs_expr.value)))
 	elif bin_expr.operator == Token.LESS_EQUALS:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value <= rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value <= rhs_expr.value)))
 	elif bin_expr.operator == Token.EQUALS_EQUALS:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value == rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value == rhs_expr.value)))
 	elif bin_expr.operator == Token.GREATER:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value > rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value > rhs_expr.value)))
 	elif bin_expr.operator == Token.GREATER_EQUALS:
-		return copy_span(bin_expr, IntExprASTNode.new(int(lhs_expr.value >= rhs_expr.value)))
+		return pop_span(IntExprASTNode.new(int(lhs_expr.value >= rhs_expr.value)))
 	
-	return copy_span(bin_expr, BinExprASTNode.new(lhs_expr, bin_expr.operator, rhs_expr))
+	return pop_span(BinExprASTNode.new(lhs_expr, bin_expr.operator, rhs_expr))
 
 
 # Fold a call expression AST node.
 func fold_call_expr(call_expr: CallExprASTNode) -> ExprASTNode:
-	var folded: CallExprASTNode = copy_span(call_expr, CallExprASTNode.new(call_expr.callee_expr))
+	push_span(call_expr)
+	var folded: CallExprASTNode = pop_span(CallExprASTNode.new(call_expr.callee_expr))
 	
 	for argument_expr in call_expr.argument_exprs:
 		folded.argument_exprs.push_back(fold_expr(argument_expr))
@@ -114,11 +121,12 @@ func fold_call_expr(call_expr: CallExprASTNode) -> ExprASTNode:
 
 # Fold an identifier expression AST node.
 func fold_identifier_expr(identifier_expr: IdentifierExprASTNode) -> ExprASTNode:
+	push_span(identifier_expr)
 	var symbol: Symbol = scope_stack.get_symbol(identifier_expr.name)
 	
 	if symbol.access == Symbol.LITERAL_INT:
-		return copy_span(identifier_expr, IntExprASTNode.new(symbol.int_value))
+		return pop_span(IntExprASTNode.new(symbol.int_value))
 	elif symbol.access == Symbol.LITERAL_STR:
-		return copy_span(identifier_expr, StrExprASTNode.new(symbol.str_value))
+		return pop_span(StrExprASTNode.new(symbol.str_value))
 	
-	return identifier_expr
+	return pop_span(identifier_expr)
