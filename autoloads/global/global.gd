ba45f0b5ebfb2c5509b682e572b64520fe7d5eb7
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-20 15:43:17 +0000

    Remove global autoload

--- autoloads/global/global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-# Global Context
-# DEPRECATED: Use `SceneManager` instead!
-
-# DEPRECATED: Use `SceneManager.change_scene` with a full scene path instead!
-func change_scene(scene_key: String, fade_out: bool = true, fade_in: bool = true) -> void:
-	SceneManager.change_scene("res://scenes/{0}/{0}.tscn".format([scene_key]), fade_out, fade_in)
