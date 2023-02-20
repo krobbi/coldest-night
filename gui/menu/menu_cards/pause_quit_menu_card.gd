215f36d57fe977e96bdc0f969aa90d25dee73de7
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-20 15:17:44 +0000

    Pause quit scene manager
    Use scene manager in pause quit menu card.

--- gui/menu/menu_cards/pause_quit_menu_card.gd
+++ gui/menu/menu_cards/pause_quit_menu_card.gd
@@ -1,8 +1,10 @@
 extends MenuCard
 
 # The pause quit menu card is a fixed menu card that is displayed when the user
-# quits to the main menu from the pause menu.
+# quits from the pause menu.
 
-# Run when the quit to main menu button is pressed. Change to the menu scene.
+export(String, FILE, "*.tscn") var _quit_scene_path: String
+
+# Run when the quit to main menu button is pressed. Change to the quit scene.
 func _on_quit_to_main_menu_button_pressed() -> void:
-	Global.change_scene("menu")
+	SceneManager.change_scene(_quit_scene_path)
