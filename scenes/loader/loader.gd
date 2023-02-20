a30741ad31133d5d8f3485d70942d7c95869f100
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-20 15:26:02 +0000

    Loader scene manager
    Use scene manager in loader scene.

--- scenes/loader/loader.gd
+++ scenes/loader/loader.gd
@@ -4,10 +4,13 @@ extends Control
 # The loader scene is a scene that loads the game from the current working save
 # data.
 
+export(String, FILE, "*.tscn") var _normal_scene_path: String
+export(String, FILE, "*.tscn") var _completed_scene_path: String
+
 var _save_data: SaveData = SaveManager.get_working_data()
 
 # Run when the loader scene is entered. Ensure the game is not paused, start the
-# new game dialog on a new game, and change to the overworld scene.
+# new game dialog on a new game, and change to the appropriate scene.
 func _ready() -> void:
 	get_tree().paused = false
 	
@@ -16,13 +19,13 @@ func _ready() -> void:
 			AudioManager.play_music("briefing")
 			EventBus.emit_nightscript_run_program_request("new_game")
 		SaveData.State.COMPLETED:
-			Global.change_scene("results")
+			SceneManager.change_scene(_completed_scene_path)
 		SaveData.State.NORMAL, _:
 			_load_normal()
 
 
-# Set the current working save data's state to normal and change to the
-# overworld scene.
+# Set the current working save data's state to normal and change to the normal
+# scene.
 func _load_normal() -> void:
 	var is_new_game: bool = _save_data.state == SaveData.State.NEW_GAME
 	_save_data.state = SaveData.State.NORMAL
@@ -31,4 +34,4 @@ func _load_normal() -> void:
 		SaveManager.push_to_slot()
 		SaveManager.save_file()
 	
-	Global.change_scene("overworld", true, false)
+	SceneManager.change_scene(_normal_scene_path, true, false)
