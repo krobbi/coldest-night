eff9f2bd5006a07f78962d9d6ed4c213b10e9434
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-02-20 00:14:42 +0000

    Change scene trigger
    Use scene manager in change scene trigger.

--- entities/triggers/change_scene_trigger/change_scene_trigger.gd
+++ entities/triggers/change_scene_trigger/change_scene_trigger.gd
@@ -4,9 +4,9 @@ extends Trigger
 # A change scene trigger is a trigger that changes the current scene when
 # entered.
 
-export(String) var _scene_key: String
+export(String, FILE, "*.tscn") var _scene_path: String
 
 # Run when a the change scene trigger is entered. Change the current scene.
 func _enter() -> void:
 	EventBus.emit_player_transition_request()
-	Global.change_scene(_scene_key)
+	SceneManager.change_scene(_scene_path)
