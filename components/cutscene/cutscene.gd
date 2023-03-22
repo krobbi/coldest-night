ba1d0eecf90f7f2e8198505ccd7b6d6cbacc59c7
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-03-22 16:27:05 +0000

    Cutscene component
    Add cutscene component.

---
+++ components/cutscene/cutscene.gd
@@ -0,0 +1,62 @@
+class_name Cutscene
+extends Node
+
+# Cutscene
+# A cutscene is a component that runs a queue of cutscene actions.
+
+signal cutscene_finished
+
+export(bool) var _is_autorun: bool = false
+
+var _current_action: CutsceneAction = null
+var _action_queue: Array = []
+
+# Run when the cutscene enters the scene tree. Disable the cutscene's physics
+# process and run the cutscene if it is an autorun.
+func _ready() -> void:
+	set_physics_process(false)
+	
+	if _is_autorun:
+		run()
+
+
+# Run on every physics frame while the cutscene has actions. Process the current
+# action.
+func _physics_process(delta: float) -> void:
+	if not _current_action:
+		if not _action_queue.empty():
+			_current_action = _action_queue.pop_front()
+			_current_action.begin()
+		else:
+			set_physics_process(false)
+			return
+	
+	if _current_action.tick(delta):
+		_current_action.end()
+		_current_action = null
+		
+		if _action_queue.empty():
+			set_physics_process(false)
+			end()
+			emit_signal("cutscene_finished")
+
+
+# Run the cutscene.
+func run() -> void:
+	nop()
+
+
+# Run when the cutscene ends.
+func end() -> void:
+	pass
+
+
+# Add an action to the cutscene.
+func add_action(action: CutsceneAction) -> void:
+	_action_queue.push_back(action)
+	set_physics_process(true)
+
+
+# Run an empty cutscene action.
+func nop() -> void:
+	add_action(CutsceneAction.new())
---
+++ components/cutscene/cutscene_action.gd
@@ -0,0 +1,20 @@
+class_name CutsceneAction
+extends Reference
+
+# Cutscene Action
+# A cutscene action is a component of a cutscene that runs an asynchronous
+# action.
+
+# Run when the cutscene action begins.
+func begin() -> void:
+	pass
+
+
+# Tick the cutscene action and return whether it has finished.
+func tick(_delta: float) -> bool:
+	return true
+
+
+# Run when the cutscene action ends.
+func end() -> void:
+	pass
