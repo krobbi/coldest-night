d129cd9d16e12abd65d3812745230290749537a7
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-03-23 01:18:32 +0000

    Unlock terminal cutscene
    Reimplement unlock terminal as cutscene.
    Remove unlock terminal scripts.
    Remove NightScript functionality from terminals.

--- entities/terminals/terminal.gd
+++ entities/terminals/terminal.gd
@@ -3,33 +3,16 @@ extends Sprite
 # Terminal
 # A terminal is an entity that runs a cutscene when interacted with.
 
-export(String) var _script_key: String = "nop"
-export(NodePath) var _cutscene_path: NodePath = NodePath()
+export(NodePath) var _cutscene_path: NodePath
 
 onready var _select_player: AudioStreamPlayer2D = $SelectPlayer
 onready var _deselect_player: AudioStreamPlayer2D = $DeselectPlayer
-
-var _cutscene: Cutscene = null
-
-# Run when the terminal finishes entering the scene tree. Get the terminal's
-# cutscene if it exists.
-func _ready() -> void:
-	if _cutscene_path and has_node(_cutscene_path) and get_node(_cutscene_path) is Cutscene:
-		_cutscene = get_node(_cutscene_path)
-
-
-# Get the terminal's Nightcript script key.
-func get_nightscript_script_key() -> String:
-	return _script_key
-
+onready var _cutscene: Cutscene = get_node(_cutscene_path)
 
 # Run when the terminal's interactable is interacted with. Run the terminal's
 # cutscene.
 func _on_interactable_interacted() -> void:
-	EventBus.emit_nightscript_run_script_request(_script_key)
-	
-	if _cutscene:
-		_cutscene.run()
+	_cutscene.run()
 
 
 # Run when the terminal's interactable is selected. Play the selected sound and
---
+++ levels/test/area_bx/west_unlock_terminal_cutscene.gd
@@ -0,0 +1,44 @@
+extends Cutscene
+
+# West Unlock Terminal Cutscene
+# The west unlock terminal cutscene is a cutscene that runs when interacting
+# with the west unlock terminal.
+
+const FLAG: String = "test/area_bx/west/is_annex_unlocked"
+
+# Run the west unlock terminal cutscene.
+func run() -> void:
+	freeze()
+	show()
+	
+	if get_flag(FLAG) != 0:
+		say("West annex is unlocked.{p=0.5}\nLock annex?")
+		option("Lock annex", "lock_option")
+	else:
+		say("West annex is locked.{p=0.5}\nUnlock annex?")
+		option("Unlock annex", "unlock_option")
+	
+	option("Cancel", "cancel_option")
+	menu()
+
+
+# Run the lock option.
+func lock_option() -> void:
+	set_flag(FLAG, 0)
+	say("Annex locked!")
+	hide()
+	unfreeze()
+
+
+# Run the unlock option.
+func unlock_option() -> void:
+	set_flag(FLAG, 1)
+	say("Annex unlocked!")
+	hide()
+	unfreeze()
+
+
+# Run the cancel option.
+func cancel_option() -> void:
+	hide()
+	unfreeze()
