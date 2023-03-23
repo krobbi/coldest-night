b0d8ebced64065e3fb417421dbb55c60893275d2
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2023-03-23 00:30:52 +0000

    Redirect terminal cutscene
    Add cutscenes section to levels.
    Reimplement redirect terminal as cutscene.
    Remove redirect terminal scripts.

---
+++ levels/test/area_bx/west_redirect_terminal_cutscene.gd
@@ -0,0 +1,31 @@
+extends Cutscene
+
+# West Redirect Terminal Cutscene
+# The west redirect terminal cutscene is the cutscene that runs when interacting
+# with the west redirect terminal.
+
+const FLAG: String = "test/area_bx/west/barrier_state"
+
+# Run the west redirect terminal cutscene.
+func run() -> void:
+	freeze()
+	show()
+	say("Redirect barriers?")
+	
+	option("Redirect barriers", "redirect_option")
+	option("Cancel", "cancel_option")
+	menu()
+
+
+# Run the redirect option.
+func redirect_option() -> void:
+	set_flag(FLAG, int(get_flag(FLAG) == 0))
+	say("Barriers redirected!")
+	hide()
+	unfreeze()
+
+
+# Run the cancel option.
+func cancel_option() -> void:
+	hide()
+	unfreeze()
