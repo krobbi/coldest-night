a53e16e7245d6918841a6ba33daaf8734558474e
krobbi <70552146+krobbi@users.noreply.github.com>
2024-01-29 16:03:42 +0000

    Remove devlog

--- gui/menu/menu_cards/main_menu_card.gd
+++ gui/menu/menu_cards/main_menu_card.gd
@@ -6,7 +6,6 @@ extends MenuCard
 
 @export_file("*.tscn") var _loader_scene_path: String
 @export_file("*.tscn") var _credits_scene_path: String
-@export_file("*.tscn") var _devlog_scene_path: String
 
 # Run when the continue game button is pressed. Pull from the slot save data and
 # change to the loader scene.
@@ -26,11 +25,6 @@ func _on_new_game_button_pressed() -> void:
 # Run when the credits button is pressed. Change to the credits scene.
 func _on_credits_button_pressed() -> void:
 	SceneManager.change_scene_to_file(_credits_scene_path)
-
-
-# Run when the devlog button is pressed. Change to the devlog scene.
-func _on_devlog_button_pressed() -> void:
-	SceneManager.change_scene_to_file(_devlog_scene_path)
 
 
 # Run when the quit game button is pressed. Quit the game.
